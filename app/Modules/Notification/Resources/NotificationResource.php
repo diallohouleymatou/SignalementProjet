5a152e542d4fb787060b513fcdf0c4d6d58810b1
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-11-17 17:12:42 +0000

    add NotificationResource for JSON representation of notifications

---
+++ app/Modules/Notification/Resources/NotificationResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Modules\Notification\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NotificationResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'type' => $this->type,
+            'title' => $this->title,
+            'message' => $this->message,
+            'data' => $this->data,
+            'action_url' => $this->action_url,
+            'icon' => $this->icon,
+            'is_read' => $this->is_read,
+            'read_at' => $this->read_at?->toISOString(),
+            'created_at' => $this->created_at?->toISOString(),
+        ];
+    }
+}

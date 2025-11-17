fe3b2766328aa80d6404992b4e4d304313990533
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-11-17 17:11:59 +0000

    register Signalement policy for authorization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Modules\Signalement\Models\Signalement;
+use App\Policies\SignalementPolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Register policies
+        Gate::policy(Signalement::class, SignalementPolicy::class);
     }
 }

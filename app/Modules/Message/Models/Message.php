28a3676c326acff8358b891a12094cefb4237c66
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-11-17 17:12:21 +0000

    add soft deletes to Message model

--- app/Modules/Message/Models/Message.php
+++ app/Modules/Message/Models/Message.php
@@ -4,10 +4,11 @@ namespace App\Modules\Message\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Message extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'signalement_id',

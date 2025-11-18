54126ad69eb4db4ae5d2f9248d6359e5f93634d7
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-11-18 09:36:13 +0000

    add MessageSent event for broadcasting message notifications

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Events;
+
+use App\Modules\Message\Models\Message;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageSent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public Message $message;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('user.' . $this->message->receiver_id),
+        ];
+    }
+}
